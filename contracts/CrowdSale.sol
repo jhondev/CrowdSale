6fdb36e039471fd15f0c8e5a75ed74c3350ae901
Jhonattan <jma200@hotmail.com>
2018-10-07 18:56:43 -0500

    crowdsale base

--- contracts/CrowdSale.sol
+++ contracts/CrowdSale.sol
@@ -19,20 +19,20 @@ contract CrowdSale {
 
     constructor(uint256 _startTime, uint256 _endTime,
         uint256 _weiTokenPrice, uint256 _etherInvestmentObjective) public {
-        require(_startTime >= now); //#A
-        require(_endTime >= _startTime); //#A
-        require(_weiTokenPrice != 0); //#A
-        require(_etherInvestmentObjective != 0); //#A
+        require(_startTime >= now); //#A1
+        require(_endTime >= _startTime); //#A1
+        require(_weiTokenPrice != 0); //#A1
+        require(_etherInvestmentObjective != 0); //#A1
 
-        startTime = _startTime; //#B
-        endTime = _endTime; //#B
-        weiTokenPrice = _weiTokenPrice; //#B
-        weiInvestmentObjective = _etherInvestmentObjective * 1000000000000000000; //#B
+        startTime = _startTime; //#B1
+        endTime = _endTime; //#B1
+        weiTokenPrice = _weiTokenPrice; //#B1
+        weiInvestmentObjective = _etherInvestmentObjective * 1000000000000000000; //#B1
 
-        crowdsaleToken = new SimpleCoin(0); //#C    
+        crowdsaleToken = new SimpleCoin(0); //#C1
         isFinalized = false;
         isRefundingAllowed = false;
-        owner = msg.sender; //#D
+        owner = msg.sender; //#D1
     }
 }
 
@@ -47,3 +47,8 @@ contract CrowdSale {
 // #I flag indicating whether refunding is allowed
 // #J account of the crowdsale contract owner
 // #K instance of the contract of the token being sold: we will use SimpleCoin for the moment
+
+// #A1 Validate input configurations
+// #B1 Set input configurations into state variables
+// #C1 Instantiate the contract of the token being sold in the crowdsale
+// #D1 Set the contract owner, as seen in SimpleCoin
