fbca61a3a445138a190f5df3b929ef50016b98ea
Jhonattan <jma200@outlook.com>
2018-10-07 19:45:01 -0500

    invest function

--- contracts/CrowdSale.sol
+++ contracts/CrowdSale.sol
@@ -1,4 +1,4 @@
-pragma solidity ^ 0.4.25;
+pragma solidity ^0.4.25;
 
 contract CrowdSale {
     uint256 public startTime; // #A
@@ -14,6 +14,9 @@ contract CrowdSale {
     bool public isFinalized; // #H
     bool public isRefundingAllowed; // #I
     address public owner; // #J
+
+    event LogInvestment(address indexed investor, uint256 value);
+    event LogTokenAssignment(address indexed investor, uint256 numTokens);
 
     SimpleCoin public crowdSaleToken; // #K
 
@@ -34,6 +37,37 @@ contract CrowdSale {
         isRefundingAllowed = false;
         owner = msg.sender; //#D1
     }
+
+    function invest() public payable { //#A2
+        require(isValidInvestment(msg.value)); //#B2
+
+        address investor = msg.sender;
+        uint256 investment = msg.value;
+
+        investmentAmountOf[investor] += investment; //#C2
+        investmentReceived += investment; //#C2
+
+        assignTokens(investor, investment); //#D2
+        LogInvestment(investor, investment); //#E2
+    }
+
+    function isValidInvestment(uint256 _investment) internal view returns(bool) { //#F2
+        bool nonZeroInvestment = _investment != 0; //#G2
+        bool withinCrowsalePeriod = now >= startTime && now <= endTime; //#H2
+
+        return nonZeroInvestment && withinCrowsalePeriod;
+    }
+
+    function assignTokens(address _beneficiary, uint256 _investment) internal {
+
+        uint256 _numberOfTokens = calculateNumberOfTokens(_investment); //#I2
+
+        crowdsaleToken.mint(_beneficiary, _numberOfTokens); //#J2
+    }
+
+    function calculateNumberOfTokens(uint256 _investment) internal returns(uint256) {
+        return _investment / weiTokenPrice; //#K2
+    }
 }
 
 // #A start time, in unix epoch, of the crowdsale funding stage
@@ -52,3 +86,15 @@ contract CrowdSale {
 // #B1 Set input configurations into state variables
 // #C1 Instantiate the contract of the token being sold in the crowdsale
 // #D1 Set the contract owner, as seen in SimpleCoin
+
+// #A2 The invest() function is declared as payable to accept ether
+// #B2 Check if the investment is valid
+// #C2 Take a record the investment contributed by each investor and of the total investment
+// #D2 Convert the ether investment into crowdsale tokens
+// #E2 Log the investment event
+// #F2 Validate investment
+// #G2 Check this is a meaningful investment
+// #H2 Check this is taking place during the crowdsale funding stage
+// #I2 Calculate the number of tokens corresponding to the investment
+// #J2 Generate the tokens in the investor account
+// #K2 Formula to calculate the number of tokens
